2a8427117b2ae5bdc00bb423a447519255c160d1
Andrew <awong21@stuy.edu>
2018-03-12 21:05:25 -0400

    finished

--- svg-bounces.js
+++ svg-bounces.js
@@ -11,6 +11,7 @@ var clear = function(){
 };
 
 var clear_total = function(){
+    // pressing clear stops any new circles from moving - works as intended
     stop();
     clear();
 };
@@ -19,33 +20,40 @@ var stop = function(){
     clearInterval(intervalID);
 };
 
-var dvd_move = function(){
-    stop()
-    var xmove = 1;
-    var ymove = 1;
-    x = 0;
-    y = 100;
-    var draw = function(){
-	clear();
-	rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-	rect.setAttribute("x", x);
-	rect.setAttribute("y", y);
-	rect.setAttribute("width", 50);
-	rect.setAttribute("height", 30);
-	rect.setAttribute("fill", "lightsteelblue");
-	svg.appendChild(rect);
-	x = x + xmove;
-	y = y + ymove;
-	if (x <= 0 || x+50 >= 500){
-	    xmove = xmove * -1;
-	}
-	if (y <= 0 || y+30 >= 500){
-	    ymove = ymove * -1;
-	}
-    };
-    intervalID = setInterval(draw, 30);
-    draw();
+var create_circle = function(e){
+	circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	circle.setAttribute("cx", e.offsetX);
+	circle.setAttribute("cy", e.offsetY);
+	circle.setAttribute("r", 30);
+    circle.setAttribute("xmove", Math.round(Math.random()) * 2 - 1);
+    circle.setAttribute("ymove", Math.round(Math.random()) * 2 - 1);
+	circle.setAttribute("fill", "lightsteelblue");
+	svg.appendChild(circle);
 };
 
-svg.addEventListener("click", dvd_move);
+var move_circles = function(){
+    for (var i = 0; i < svg.children.length; i++){
+        var circle = svg.children[i];
+        // getAttribute() returns a string, must convert to int
+        var cx = parseInt(circle.getAttribute("cx"));
+        var cy = parseInt(circle.getAttribute("cy"));
+        var xmove = parseInt(circle.getAttribute("xmove"));
+        var ymove = parseInt(circle.getAttribute("ymove"));
+        
+        if (cx <= 30 || cx >= 470){
+            circle.setAttribute("xmove", xmove * -1);
+            xmove = xmove * -1;
+        };
+        if (cy <= 30 || cy >= 470){
+            circle.setAttribute("ymove", ymove * -1);
+            ymove = ymove * -1;
+        };
+        
+        circle.setAttribute("cx", cx + xmove);
+        circle.setAttribute("cy", cy + ymove);
+    };
+};
+
+intervalID = setInterval(move_circles, 10);
+svg.addEventListener("click", create_circle);
 b1.addEventListener("click", clear_total);
